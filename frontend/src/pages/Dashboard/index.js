cc425d5eb477da8501256dc47e2387b5b104ed7a
Roger M. Brusamarello <34141109+RogerBrusamarello@users.noreply.github.com>
2019-10-06 18:53:07 -0300

    Coreção do Texto no Dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -50,7 +50,7 @@ export default function Dashboard() {
                 {requests.map(request => (
                     <li key={request._id}>
                         <p>
-                            <strong>{request.user.email}</strong> está solicitando uma reserva em
+                            <strong>{request.user.email}</strong> está solicitando uma reserva em 
                         <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>
                         </p>
                         <button className="accept" onClick={() => handleAccept(request._id)} >ACEITAR</button>
